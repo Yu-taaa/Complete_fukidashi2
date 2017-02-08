241e72aa2bebedb49d08712723fdb1ef6d0b099a
konndoyuta <konndoyuta@konndoyuta-no-MacBook-Pro.local>
2017-02-08 19:44:59 +0900

    add ray

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RayCast();
+        }
+    }
+
+    void RayCast()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            var character = hit.collider.gameObject.GetComponent<character>();
+            if (character == null)
+            {
+                return;
+
+            }
+            character.StartConversation();
+        }
+    }
+}
+
+//prefab化する理由は？
